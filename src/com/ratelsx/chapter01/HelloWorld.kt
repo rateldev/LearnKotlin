5b915271125c2b8215ccaeb7a56c4d66354aa733
rateldev <350790090@qq.com>
2022-01-25 16:54:56 +0800

    HelloWorld

---
+++ src/com/ratelsx/chapter01/HelloWorld.kt
@@ -0,0 +1,12 @@
+package com.ratelsx.chapter01
+
+/**
+ * @author Ratel
+ * @date 2022/1/25
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ */
+fun main(args: Array<String>) {
+    println("Hello World")
+}
