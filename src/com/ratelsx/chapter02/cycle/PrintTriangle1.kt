969feb83e6d64e7522ebfd007938599cca05a855
rateldev <350790090@qq.com>
2022-01-26 11:53:15 +0800

    for循环语句

---
+++ src/com/ratelsx/chapter02/cycle/PrintTriangle1.kt
@@ -0,0 +1,18 @@
+package com.ratelsx.chapter02.cycle
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * for循环语句
+ */
+fun main(args: Array<String>) {
+    // 循环4次，且步长1的递增，0..3表示[0,3]之间的数字
+    // i的值会在0~3变化
+    // 循环语句只有一条时，可以进行简写
+    for (i in 0..3) println("i=>$i")
+
+}
