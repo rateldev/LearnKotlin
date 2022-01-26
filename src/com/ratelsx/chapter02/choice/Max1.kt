806d82229c3ddaae6c47c715c963586cc00ac9b7
rateldev <350790090@qq.com>
2022-01-26 10:18:18 +0800

    if语句

---
+++ src/com/ratelsx/chapter02/choice/Max1.kt
@@ -0,0 +1,20 @@
+package com.ratelsx.chapter02.choice
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * if语句
+ */
+fun main(args: Array<String>) {
+    val max: Int
+    val a = 5
+    val b = 6
+    if (a < b) {
+        max = b
+        println("max=$max")
+    }
+}
