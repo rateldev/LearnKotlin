90b376479569a780fda8651bcf908c914aaee62a
rateldev <350790090@qq.com>
2022-01-26 09:59:58 +0800

    使用模板表达式输出

---
+++ src/com/ratelsx/chapter02/string/LiteralValues.kt
@@ -0,0 +1,14 @@
+package com.ratelsx.chapter02.string
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ * 使用模板表达式输出$
+ */
+fun main(args: Array<String>) {
+    val price = """${'$'}8.88"""
+    println(price)
+}
