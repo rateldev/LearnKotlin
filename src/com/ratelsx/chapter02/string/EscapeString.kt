845460538faa5c1fb225c989350989e3675e0959
rateldev <350790090@qq.com>
2022-01-26 09:44:03 +0800

    转义字符串

---
+++ src/com/ratelsx/chapter02/string/EscapeString.kt
@@ -0,0 +1,16 @@
+package com.ratelsx.chapter02.string
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * 字符串字面值
+ */
+fun main(args: Array<String>) {
+    // 字符串中包含转义字符
+    val str = "您\n好"
+    println(str)
+}
