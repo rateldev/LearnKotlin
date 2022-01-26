79cc162db52a4ca96c7b32c5f7f1754df168ad6d
rateldev <350790090@qq.com>
2022-01-26 18:24:34 +0800

    as操作符

---
+++ src/com/ratelsx/chapter02/variable/AsOperator.kt
@@ -0,0 +1,17 @@
+package com.ratelsx.chapter02.variable
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * as操作符
+ */
+fun main(args: Array<String>) {
+    val a = "1"
+    // 将字符串类型变量a强制转换为String类型
+    val b: String = a as String
+    print(b.length)
+}
