6001e682b3f99fcacbe185ee18dfd6d8993c5372
rateldev <350790090@qq.com>
2022-01-26 16:30:45 +0800

    until函数

---
+++ src/com/ratelsx/chapter02/range/Until.kt
@@ -0,0 +1,16 @@
+package com.ratelsx.chapter02.range
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * until函数
+ */
+fun main(args: Array<String>) {
+    for (i in 1 until 4) {
+        print(i.toString() + "\t")
+    }
+}
