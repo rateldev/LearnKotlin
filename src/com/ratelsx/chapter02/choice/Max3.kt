212b5d57db5cf2647616172e73e5a4d073a791e9
rateldev <350790090@qq.com>
2022-01-26 11:04:12 +0800

    三元运算符

---
+++ src/com/ratelsx/chapter02/choice/Max3.kt
@@ -0,0 +1,18 @@
+package com.ratelsx.chapter02.choice
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * 三元运算符
+ */
+fun main(args: Array<String>) {
+    val max: Int
+    val a = 5
+    val b = 6
+    max = if (a > b) a else b
+    println("max=$max")
+}
