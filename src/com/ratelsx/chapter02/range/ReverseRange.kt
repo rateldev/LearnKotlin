51297a17649da60d8fd22ff05fb2244781dcfdca
rateldev <350790090@qq.com>
2022-01-26 16:32:29 +0800

    逆向区间

---
+++ src/com/ratelsx/chapter02/range/ReverseRange.kt
@@ -0,0 +1,16 @@
+package com.ratelsx.chapter02.range
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * 逆向区间
+ */
+fun main(args: Array<String>) {
+    for (i in 4 downTo 1) {
+        print(i.toString() + "\t")
+    }
+}
