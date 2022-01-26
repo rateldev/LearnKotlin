fabfc33a5fcd2cac1b7ecfdb9ed8a243f59b5fdf
rateldev <350790090@qq.com>
2022-01-26 16:53:21 +0800

    for循环遍历数组

---
+++ src/com/ratelsx/chapter02/array/ThroughArray.kt
@@ -0,0 +1,17 @@
+package com.ratelsx.chapter02.array
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * for循环遍历数组
+ */
+fun main(args: Array<String>) {
+    val intArr = intArrayOf(1, 2, 3, 4)
+    for (i in intArr) {
+        println(i)
+    }
+}
