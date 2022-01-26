ff5f57dc5674105adc5be177f6ad57911b023b28
rateldev <350790090@qq.com>
2022-01-26 16:02:35 +0800

    do…while循环

---
+++ src/com/ratelsx/chapter02/cycle/PrintNum2.kt
@@ -0,0 +1,18 @@
+package com.ratelsx.chapter02.cycle
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * do…while循环
+ */
+fun main(args: Array<String>) {
+    var n = 5
+    do {
+        println("n=$n")
+        n--
+    } while (n > 0)
+}
