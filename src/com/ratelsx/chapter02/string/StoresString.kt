2e3147b4182721a5b928e11073c97e9dfb56fc7c
rateldev <350790090@qq.com>
2022-01-26 09:55:27 +0800

    模板表达式

---
+++ src/com/ratelsx/chapter02/string/StoresString.kt
@@ -0,0 +1,18 @@
+package com.ratelsx.chapter02.string
+
+/**
+ * @author Ratel
+ * @date 2022/1/26
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * 模板表达式
+ */
+fun main(args: Array<String>) {
+    val a = 1
+    // 语法格式${变量}
+    // 可以省略大括号
+    val s1 = "a is $a"
+    println(s1)
+}
