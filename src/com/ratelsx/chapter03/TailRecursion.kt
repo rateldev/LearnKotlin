08aa84747d56df05348ab4149a1a351e15bd9c53
rateldev <350790090@qq.com>
2022-01-27 18:19:20 +0800

    尾递归函数

---
+++ src/com/ratelsx/chapter03/TailRecursion.kt
@@ -0,0 +1,24 @@
+package com.ratelsx.chapter03
+
+/**
+ * @author Ratel
+ * @date 2022/1/27
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * 尾递归函数
+ */
+/*
+fun main(args: Array<String>) {
+    // 调用尾递归函数
+    println(sum(100))
+}
+// 尾递归函数
+fun sum(num: Int, total: Int = 0): Int {
+    return if (num == 1) {
+        1 + total
+    } else {
+        sum(num - 1, num + total)
+    }
+}*/
