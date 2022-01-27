b097afb8b8306234d05d424b193443481053efd2
rateldev <350790090@qq.com>
2022-01-27 17:59:16 +0800

    递归函数

--- src/com/ratelsx/chapter03/Recursive.kt
+++ src/com/ratelsx/chapter03/Recursive.kt
@@ -10,41 +10,37 @@ package com.ratelsx.chapter03
  * 递归函数
  */
 
-///**
-// * 递归函数
-// */
-//fun main(args: Array<String>) {
-//    // 调用递归函数
-//    println(sum(4))
-//}
-//
-//// 定义一个sum()函数
-//fun sum(num: Int): Int {
-//    // num为1时，则指定返回值为1
-//    return if (num == 1) {
-//        1
-//    } else {
-//        // num不为1时，返回num与sum()返回值之和
-//        num + sum(num - 1)
-//    }
-//}
+/*
+fun main(args: Array<String>) {
+    // 调用递归函数
+    println(sum(4))
+}
 
-///*
-//*
-// * 尾递归函数
-//fun main(args: Array<String>) {
-//    // 调用尾递归函数
-//    println(sum(100))
-//}
-//
-//// 尾递归函数
-//fun sum(num: Int, total: Int = 0): Int {
-//    return if (num == 1) {
-//        1 + total
-//    } else {
-//        sum(num - 1, num + total)
-//    }
-//}
+// 定义一个sum()函数
+fun sum(num: Int): Int {
+    // num为1时，则指定返回值为1
+    return if (num == 1) {
+        1
+    } else {
+        // num不为1时，返回num与sum()返回值之和
+        num + sum(num - 1)
+   }
+}*/
+
+/*
+尾递归函数
+fun main(args: Array<String>) {
+    // 调用尾递归函数
+    println(sum(100))
+}
+// 尾递归函数
+fun sum(num: Int, total: Int = 0): Int {
+    return if (num == 1) {
+        1 + total
+    } else {
+        sum(num - 1, num + total)
+    }
+}*/
 
 /**
  * 尾递归函数的优化
