b3297f8b0e3331ce23e95c9be69de098ef49aa89
rateldev <350790090@qq.com>
2022-01-27 16:53:31 +0800

    顶层函数

---
+++ src/cn/ratelsx/chapter03/StudentInfo.kt
@@ -0,0 +1,15 @@
+package cn.ratelsx.chapter03
+
+/**
+ * @author Ratel
+ * @date 2022/1/27
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * 顶层函数
+ */
+fun stuInfo(name: String, age: Int) {
+    println("姓名：$name")
+    println("年龄：$age")
+}
