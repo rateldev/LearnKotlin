d78c25c2ac5485a5e96fdb8a370f5d735973e6b7
rateldev <350790090@qq.com>
2022-02-07 17:54:47 +0800

    类的继承、方法重写、super

--- src/com/ratelsx/chapter04/Family.kt
+++ src/com/ratelsx/chapter04/Family.kt
@@ -7,16 +7,24 @@ package com.ratelsx.chapter04
  * WebSite：https://ratelsx.com
  * email：ratels@foxmail.com
  *
- * 类的继承
+ * 类的继承、方法重写、super
  */
 open class Father {
-    fun sayHello() {
-        println("Hello")
+    open var name = "江小白"
+    open var age = 35
+    open fun sayHello() {
+        println("Hello!我叫$name,我今年$age 岁。")
     }
 }
 
 class Son : Father() {
+    override var name = "小小白"
+    override var age = 5
 
+    override fun sayHello() {
+        super.sayHello()
+        println("Hello!我是江小白的儿子,我叫$name,我今年$age 岁。")
+    }
 }
 
 fun main(args: Array<String>) {
