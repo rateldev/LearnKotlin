d217a8785d9881ffa24845fd17a1d8a1e912c185
rateldev <350790090@qq.com>
2022-02-08 11:14:08 +0800

    枚举类

---
+++ src/com/ratelsx/chapter04/Week1.kt
@@ -0,0 +1,20 @@
+package com.ratelsx.chapter04
+
+/**
+ * @author Ratel
+ * @date 2022/2/8
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * 枚举类
+ */
+enum class Week1(val what: String, val doSomeThing: String) {
+    MONDAY("星期一", "上班"),
+    TUESDAY("星期二", "聚会"),
+    WEDNESDAY("星期三", "上班"),
+    THURSDAY("星期四", "上班"),
+    FRIDAY("星期五", "上班"),
+    SATURDAY("星期六", "聚会"),
+    SUNDAY("星期日", "聚会")
+}
