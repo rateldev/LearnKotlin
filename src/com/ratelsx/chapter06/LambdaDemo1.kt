9a21d5e6b2ff430f9e802675baff5e743a7a906e
Ratel <350790090@qq.com>
2022-02-09 15:23:56 +0800

    无参数有返回值Lambda表达式

---
+++ src/com/ratelsx/chapter06/LambdaDemo1.kt
@@ -0,0 +1,17 @@
+package com.ratelsx.chapter06
+
+/**
+ * @author Ratel
+ * @date 2022/2/9
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * 无参数有返回值Lambda表达式
+ */
+fun main(args: Array<String>) {
+    {
+        // 这一行就是函数体
+        println("Lambda表达式无参数有返回值")
+    }()
+}
