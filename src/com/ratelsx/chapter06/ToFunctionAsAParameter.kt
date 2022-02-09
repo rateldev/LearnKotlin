a5e4ef7951732a876e38536ed10ec017bb7fee37
Ratel <350790090@qq.com>
2022-02-09 16:48:53 +0800

    函数作为参数使用以及优化

--- src/com/ratelsx/chapter06/ToFunctionAsAParameter.kt
+++ src/com/ratelsx/chapter06/ToFunctionAsAParameter.kt
@@ -7,10 +7,16 @@ package com.ratelsx.chapter06
  * WebSite：https://ratelsx.com
  * email：ratels@foxmail.com
  *
- * 函数作为参数使用
+ * 函数作为参数使用以及优化
+ *
+ * 1.省略小括号
+ * 2.将参数移动到小括号外面
+ * 3.使用it关键字
  */
 
+// 给区间定义一个扩展方法
 fun IntRange.pickNum(function: (Int) -> Boolean): List<Int> {
+// fun IntRange.pickNum(need: Int, function: (Int) -> Boolean): List<Int> {
     // 声明集合
     val resultList = mutableListOf<Int>()
     // this指向定义的区间(IntRange)范围1~20
@@ -26,9 +32,21 @@ fun IntRange.pickNum(function: (Int) -> Boolean): List<Int> {
 }
 
 fun main(args: Array<String>) {
-    val list = 1..20
-    println("--------能被5整除的数--------")
-    println(list.pickNum { x: Int -> x % 5 == 0 })
+    // 定义区间范围1~20
+    val list = 1..50
     println("--------能被10整除的数--------")
+    // 省略之前
+    // println(list.pickNum({ x: Int -> x % 10 == 0 }))
+    // 省略之后
+    // println(list.pickNum { x: Int -> x % 10 == 0 })
+
+    // 将参数从括号中移出之前
+    // println(list.pickNum(1, { x: Int -> x % 10 == 0 }))
+    // 将参数从括号中移出之后
+    // println(list.pickNum(1) { x: Int -> x % 10 == 0 })
+
+    // 使用it关键字之前
     println(list.pickNum { x: Int -> x % 10 == 0 })
+    // 使用it关键字之后
+    println(list.pickNum { it % 10 == 0 })
 }
