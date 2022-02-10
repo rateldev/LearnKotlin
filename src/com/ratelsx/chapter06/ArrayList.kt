56acece69f0b6a7adb3d7ce7e476adb56344ae51
Ratel <350790090@qq.com>
2022-02-10 10:10:31 +0800

    ArrayList集合添加数据

---
+++ src/com/ratelsx/chapter06/ArrayList.kt
@@ -0,0 +1,18 @@
+package com.ratelsx.chapter06
+
+/**
+ * @author Ratel
+ * @date 2022/2/10
+ * GitHub：https://github.com/rateldev
+ * WebSite：https://ratelsx.com
+ * email：ratels@foxmail.com
+ *
+ * ArrayList集合添加数据
+ */
+fun main(args: Array<String>) {
+    val list = ArrayList<Int>()
+    list.add(1)
+    list.add(2)
+    list.add(3)
+    println(list)
+}
